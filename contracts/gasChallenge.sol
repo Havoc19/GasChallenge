72f8eae286d5d6ae756315d29c6a43ed68d18e9f
havoc19 <akarshmaurya19@gmail.com>
2023-07-20 22:57:19 +0530

    gas challenge - optimized function

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "hardhat/console.sol";
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
     
+
     //Function to check for sum of array
     //No changes required in this function
     function getSumOfArray() public view returns (uint256) {
@@ -24,6 +26,15 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+      for (uint i = 0; i < numbers.length; ++i) {
+            unchecked{
+                numbers[i] = 0;
+            }
+        }
+    }
+
+    //Another way to make sum of array zero - according to me
+    function optimizedFunction2() public {
+      delete numbers;
     }
  }
--- test/test_gasChallenge.js
+++ test/test_gasChallenge.js
@@ -15,12 +15,19 @@ describe("Deploy Gas Challenge Contract", () => {
     it("Should return lower gas", async () => {
       await gas_contract.notOptimizedFunction();
       await gas_contract.optimizedFunction();
+      await gas_contract.optimizedFunction2();
     });
   });
 
   describe("Check Sum Of Array", () => {
     it("Should return 0", async () => {
-      // Write test block here to check sum of array equals 0
+      await gas_contract.optimizedFunction();
+      expect(await gas_contract.getSumOfArray()).to.be.equals(Number(0));
+    });
+
+    it("Should return 0 by second technique", async () => {
+      await gas_contract.optimizedFunction2();
+      expect(await gas_contract.getSumOfArray()).to.be.equals(Number(0));
     });
   });
 });
