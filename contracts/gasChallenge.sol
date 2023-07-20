affb8a17c6d06e78fa47b0353d3186db80063c55
havoc19 <akarshmaurya19@gmail.com>
2023-07-20 23:15:44 +0530

    more optimizations in function

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -4,8 +4,7 @@ import "hardhat/console.sol";
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
-    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
-    
+    uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     //Function to check for sum of array
     //No changes required in this function
@@ -16,18 +15,18 @@ contract gasChallenge {
         }
         return sum;
     }
-    
+
     function notOptimizedFunction() public {
         for (uint i = 0; i < numbers.length; i++) {
             numbers[i] = 0;
         }
     }
-    
+
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      for (uint i = 0; i < numbers.length; ++i) {
-            unchecked{
+        unchecked {
+            for (uint i = 0; i < numbers.length; ++i) {
                 numbers[i] = 0;
             }
         }
@@ -35,6 +34,6 @@ contract gasChallenge {
 
     //Another way to make sum of array zero - according to me
     function optimizedFunction2() public {
-      delete numbers;
+        delete numbers;
     }
- }
+}
